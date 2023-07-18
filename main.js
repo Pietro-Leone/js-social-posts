ac1fe5610461f549692ea02f54ad1279a8d558c4
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-18 15:31:31 +0200

    stampati i post nell'HTML

--- main.js
+++ main.js
@@ -50,3 +50,40 @@ const posts = [{
     "created": "2021-03-05"
 }
 ];
+const container = document.getElementById("container");
+
+posts.forEach((post, i) => {
+    const singleAuthor = post.author;
+    let posts = document.createElement("div");
+    posts.classList.add("post");
+    posts.innerHTML = `<div class="post__header">
+                            <div class="post-meta">
+                                <div class="post-meta__icon">
+                                    <img class="profile-pic" src="${singleAuthor.image}" alt="${singleAuthor.name}">
+                                </div>
+                                <div class="post-meta__data">
+                                    <div class="post-meta__author">${singleAuthor.name}</div>
+                                    <div class="post-meta__time">${post.created}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="post__text">${post.content}</div>
+                        <div class="post__image">
+                            <img src="${post.media}" alt="media">
+                        </div>
+                        <div class="post__footer">
+                            <div class="likes js-likes">
+                                <div class="likes__cta">
+                                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                        <span class="like-button__label">Mi Piace</span>
+                                    </a>
+                                </div>
+                                <div class="likes__counter">
+                                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                                </div>
+                            </div>
+                        </div>`
+    container.append(posts);
+
+});
